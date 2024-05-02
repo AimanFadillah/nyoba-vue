a8b25cd1f7a5c400025a804ca5d0d55506275fa3
Aiman Fadillah <mlakece914@gmail.com>
2024-05-02 20:55:19 +0700

    template syntax

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
-<script setup>
+<!-- <script setup>
+  import { ref } from 'vue';
+  const loop = ref(0)
+</script> -->
 
+<script>
+  export default {
+
+    data(){
+      return {
+        loop:0,
+        image:"ending.png",
+        hidden:true,
+      }
+    },
+
+    methods:{
+      tambah(){
+        this.loop++
+      },
+      toggleHidden(){
+        this.hidden = !this.hidden;
+      }
+    },
+
+    created(){  
+      console.log("first")
+    },
+
+    mounted(){
+      console.log("Hai")
+    }
+  }
 </script>
 
 <template>
   <main>
-    <h1>Hello Word</h1>
+    <h1 @click="tambah" >Hello Word {{ loop }}</h1>
+    <h1 v-if="!hidden" >Aku bersembunyi</h1>
+    <img @click="toggleHidden" v-bind:src="`/images/${image}`" />
   </main>
 </template>
