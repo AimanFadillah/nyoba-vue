b87c9714d4dda85cc651f69c2a54f98e30e6a2e2
Aiman Fadillah <mlakece914@gmail.com>
2024-05-06 21:13:03 +0700

    component teleport

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup >
   import { reactive, ref } from 'vue';
   import local from '@/components/local.vue';
+  import componetTeleport from '@/components/componetTeleport.vue';
 
   const loop = ref(1)
   const image = ref("ending.png")
@@ -25,6 +26,7 @@
 <template>
   <main>
     <h1 @click="tambah" :class="{'text-danger':loop % 2 === 0}" :style="{'font-size':20 * loop + 'px'}" >Klik saya yang ke {{ loop - 1 }}</h1>
+    <componetTeleport />
     <h1 v-if="!hidden" >Baa</h1>
     <h1 v-else  >Ciluk</h1>
     <ul>
